be83f780ae8f533d2326d6518bab572a96053abb
obrunodev <brunorpdev@gmail.com>
2021-02-16 09:08:54 -0300

    Primeiro commit, primeiro teste de consumo da API

---
+++ app.py
@@ -0,0 +1,10 @@
+import requests
+
+pokemon = input("Digite o nome de um pokemon: ")
+url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+req = requests.get(url)
+poke = req.json()
+
+texto = f"O pokemon escolhido é o {poke['name']} e ele pesa {poke['height']}kg."
+
+print(texto)
