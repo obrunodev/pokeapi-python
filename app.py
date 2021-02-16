d6a50d4fc455d556e9cb518e87a4c268521cabaf
obrunodev <brunorpdev@gmail.com>
2021-02-16 09:11:23 -0300

    Tratamento de dados simples adicionado

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import requests
 pokemon = input("Digite o nome de um pokemon: ")
 url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
 req = requests.get(url)
-poke = req.json()
 
-texto = f"O pokemon escolhido é o {poke['name']} e ele pesa {poke['height']}kg."
-
-print(texto)
+try:
+  poke = req.json()
+  texto = f"O pokemon escolhido é o {poke['name']} e ele pesa {poke['height']}kg."
+  print(texto)  
+except:
+  print("Pokemon não encotrado.")
